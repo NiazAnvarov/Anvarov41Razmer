b94a938d2b9b516e195317d4c20afc9750b944e8
Niaz <niazanvarov24@gmail.com>
2024-02-12 22:06:08 +0500

    18.1

--- Anvarov41Razmer/ProductPage.xaml.cs
+++ Anvarov41Razmer/ProductPage.xaml.cs
@@ -20,6 +20,10 @@ namespace Anvarov41Razmer
     /// </summary>
     public partial class ProductPage : Page
     {
+
+        int maxCount = 0;
+        List<Product> TableList;
+
         public ProductPage()
         {
             InitializeComponent();
@@ -27,11 +31,79 @@ namespace Anvarov41Razmer
             List<Product> currentProducts = Anvarov41Entities.GetContext().Product.ToList();
             ProductListView.ItemsSource = currentProducts;
 
+            TableList = Anvarov41Entities.GetContext().Product.ToList();
+            maxCount = TableList.Count;
+            MCount = maxCount;
+
+            Filter.SelectedIndex = 0;
+
+            Update();
+
+        }
+
+        private void Update()
+        {
+            var currentProducts = Anvarov41Entities.GetContext().Product.ToList();
+
+            if(Filter.SelectedIndex == 0)
+            {
+                currentProducts = currentProducts.Where(p => p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount <= 100).ToList();
+            }
+
+            if (Filter.SelectedIndex == 1)
+            {
+                currentProducts = currentProducts.Where(p => p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount <= 9.99).ToList();
+            }
+
+            if (Filter.SelectedIndex == 2)
+            {
+                currentProducts = currentProducts.Where(p => p.ProductDiscountAmount >= 10 && p.ProductDiscountAmount <= 14.99).ToList();
+            }
+
+            if (Filter.SelectedIndex == 3)
+            {
+                currentProducts = currentProducts.Where(p => p.ProductDiscountAmount >= 15 && p.ProductDiscountAmount <= 100).ToList();
+            }
+
+            if (RButtonUp.IsChecked.Value)
+            {
+                currentProducts = currentProducts.OrderBy(p => p.ProductCost).ToList();
+            }
+
+            if (RButtonDown.IsChecked.Value)
+            {
+                currentProducts = currentProducts.OrderByDescending(p => p.ProductCost).ToList();
+            }
+
+            currentProducts = currentProducts.Where(p => p.ProductName.ToLower().Contains(Search.Text.ToLower())).ToList();
+
+            ProductListView.ItemsSource = currentProducts;
+
         }
 
         private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
 
+        }
+
+        private void Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Update();
+        }
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            Update();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            Update();
+        }
+
+        private void Filter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
         }
     }
 }
